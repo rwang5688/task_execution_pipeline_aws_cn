6f0530be09e20b477d5ea1aa385541e1c204e422
Robert Wang <rwang5688@outlook.com>
2020-11-22 23:46:11 -0800

    revert changes

--- submit_task/env.sh
+++ submit_task/env.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 # AWS specific environment variables
-export AWS_ACCOUNT_ID=700702834148
-export TARGET_CLOUD=aws
-export TARGET_REGION=us-west-2
+export AWS_ACCOUNT_ID=200691530094
+export TARGET_CLOUD=aws-cn
+export TARGET_REGION=cn-northwest-1
 
 # task-list workflow and database specific environment variables
-export TASK_EXEC_PREPROCESS_DATA_BUCKET=task-exec-preprocess-data-bucket-rwang5688
-export TASK_EXEC_LOG_DATA_BUCKET=task-exec-log-data-bucket-rwang5688
-export TASK_EXEC_RESULT_DATA_BUCKET=task-exec-result-data-bucket-rwang5688
-export TASK_EXEC_CREATE_TASK_QUEUE=task-exec-create-task-queue-rwang5688
-export TASK_EXEC_PROCESS_TASK_QUEUE=task-exec-process-task-queue-rwang5688
-export TASK_EXEC_UPDATE_TASK_QUEUE=task-exec-update-task-queue-rwang5688
-export TASK_EXEC_UPLOAD_TASK_ISSUES_QUEUE=task-exec-upload-task-issues-queue-rwang5688
-export TASK_EXEC_GENERATE_TASK_SUMMARY_QUEUE=task-exec-generate-task-summary-queue-rwang5688
-export TASK_EXEC_TASK_TABLE=task-exec-task-table-rwang5688
-export TASK_EXEC_ISSUE_TABLE=task-exec-issue-table-rwang5688
+export TASK_EXEC_PREPROCESS_DATA_BUCKET=task-exec-preprocess-data-bucket-xcalibyte-com-cn
+export TASK_EXEC_LOG_DATA_BUCKET=task-exec-log-data-bucket-xcalibyte-com-cn
+export TASK_EXEC_RESULT_DATA_BUCKET=task-exec-result-data-bucket-xcalibyte-com-cn
+export TASK_EXEC_CREATE_TASK_QUEUE=task-exec-create-task-queue-xcalibyte-com-cn
+export TASK_EXEC_PROCESS_TASK_QUEUE=task-exec-process-task-queue-xcalibyte-com-cn
+export TASK_EXEC_UPDATE_TASK_QUEUE=task-exec-update-task-queue-xcalibyte-com-cn
+export TASK_EXEC_UPLOAD_TASK_ISSUES_QUEUE=task-exec-upload-task-issues-queue-xcalibyte-com-cn
+export TASK_EXEC_GENERATE_TASK_SUMMARY_QUEUE=task-exec-generate-task-summary-queue-xcalibyte-com-cn
+export TASK_EXEC_TASK_TABLE=task-exec-task-table-xcalibyte-com-cn
+export TASK_EXEC_ISSUE_TABLE=task-exec-issue-table-xcalibyte-com-cn
 
